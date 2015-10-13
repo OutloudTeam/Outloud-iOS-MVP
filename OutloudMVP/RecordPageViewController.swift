ef14be37ce18bac2d6659da59652ef57c95cdd3d
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-10-12 23:26:43 -0600

    added record page view controller

--- OutloudMVP/RecordPageViewController.swift
+++ OutloudMVP/RecordPageViewController.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import UIKit
+
+class RecordPageViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad();
+        
+        // Bottom View
+        let bottomView = UIView();
+        bottomView.backgroundColor = controlBarColor;
+        self.view.addSubview(bottomView);
+        
+        bottomView.snp_makeConstraints { (make) -> Void in
+            make.width.equalTo(self.view);
+            make.height.equalTo(80);
+            make.bottom.equalTo(self.view);
+        };
+        
+        
+        // Progress View
+        let progressView = UIView();
+        progressView.layer.borderColor = progressBarColor.CGColor;
+        progressView.layer.borderWidth = 0.5;
+        self.view.addSubview(progressView);
+        
+        progressView.snp_makeConstraints{ (make) -> Void in
+            make.width.equalTo(self.view);
+            make.height.equalTo(20);
+            make.bottom.equalTo(bottomView.snp_top);
+        };
+        
+        let filledProgress = UIView();
+        filledProgress.backgroundColor = progressBarColor;
+        progressView.addSubview(filledProgress);
+        filledProgress.snp_makeConstraints{ (make) -> Void in
+            make.width.equalTo(progressView).dividedBy(4);
+            make.height.top.left.equalTo(progressView);
+        }
+        
+        let progressLabel = UILabel();
+        progressLabel.text = "Section 1";
+        progressLabel.font = UIFont(name: "Avenir-Light", size: 10);
+        progressLabel.textColor = UIColor.blackColor();
+        progressView.addSubview(progressLabel);
+        
+        progressLabel.snp_makeConstraints{ (make) -> Void in
+            make.left.equalTo(progressView).offset(5);
+            make.top.height.equalTo(progressView);
+            make.width.equalTo(progressView).offset(64);
+        };
+        
+        
+        // Article Info Section
+    }
+    
+}
